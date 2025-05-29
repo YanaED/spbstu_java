39e0d8e5b202bb78a67c0b20355dfc2e4681c5d8
yanaed <ydegorova@gmail.com>
2025-05-29 22:52:50 +0300

    initial commit

---
+++ src/main/java/com/example/java_spbstu/JavaSpbstuApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spbstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpbstuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpbstuApplication.class, args);
+    }
+
+}
